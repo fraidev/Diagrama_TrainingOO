f267e85d010ed2c30ca1f3dab8bad5b2cd2c8d01
Frai <fraifelipe@gmail.com>
2018-07-25 22:12:33 -0300

    Init2

---
+++ Diagrama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using Diagrama.Domain;
+using Diagrama.Domain.Sorts;
+
+namespace Diagrama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aluno = new Aluno("Felipe",10);
+            var aluno2= new Aluno("Felipe",10);
+            var automoveis = new Automoveis();
+            var selection = new SelectionSort();
+            selection.Ordenar(aluno.CompararCom(aluno2));
+        }
+    }
+}
